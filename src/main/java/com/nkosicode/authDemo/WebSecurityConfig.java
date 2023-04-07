1689793ee53f6127a3b835e55923c543f5b8c8d6
Bonga-X01 <bbonginkosi18@gmail.com>
2023-04-07 23:06:17 +0200

    Fixed authentication failure

--- src/main/java/com/nkosicode/authDemo/WebSecurityConfig.java
+++ src/main/java/com/nkosicode/authDemo/WebSecurityConfig.java
@@ -1,28 +1,31 @@
 package com.nkosicode.authDemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Configuration
 public class WebSecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-        throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin();
+
         return http.build();
     }
+
     @Autowired
-    public void configure(AuthenticationManagerBuilder auth)
-        throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .ldapAuthentication()
                 .userDnPatterns("uid={0},ou=people")
@@ -34,4 +37,19 @@ public class WebSecurityConfig {
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
     }
+
 }
+
+//@Configuration
+//public class WebSecurityConfig {
+
+//  @Bean
+//  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//    return http
+//      .authorizeRequests()
+//      .anyRequest().authenticated()
+//      .and()
+//      .formLogin(Customizer.withDefaults())
+//      .build();
+//  }
+//}
